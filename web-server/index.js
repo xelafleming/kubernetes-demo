f4a47d929d075ac09d84ca56c2ddc00b4f3b12f8
Alex Fleming <alex@alexfleming.dev>
2020-01-30 09:18:05 +0000

    update logging

--- web-server/index.js
+++ web-server/index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.listen(3000, () => console.log("Listening on port 3000..."));
 
-app.get("/identity", (req, res, _next) => {
-  console.log("Recieved request on /identity from", req.headers.host);
+app.get("/identity", (_req, res, _next) => {
+  console.log("Recieved request on /identity");
   res.json({ podIdentifier: process.env.HOSTNAME });
 });
