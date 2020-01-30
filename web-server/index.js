b06ea247e5188b225a3119e07b36fab294cf6daf
Alex Fleming <alex@alexfleming.dev>
2020-01-30 09:12:55 +0000

    Delete unused deployment yaml, add deployment strategy to web server, add logging in web server

--- web-server/index.js
+++ web-server/index.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.listen(3000, () => console.log("Listening on port 3000..."));
 
-app.get("/identity", (_req, res, _next) => {
+app.get("/identity", (req, res, _next) => {
+  console.log("Recieved request on /identity from", req.headers.host);
   res.json({ podIdentifier: process.env.HOSTNAME });
 });
